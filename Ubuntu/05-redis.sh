faa9ce0d6a37535164816b9cbd2b7e9a3d842a94
ermouth <ermouth@gmail.com>
2024-07-08 18:36:38 -0700

    create redis user

--- Ubuntu/05-redis.sh
+++ Ubuntu/05-redis.sh
@@ -15,6 +15,13 @@ BASEDIRPATH=$(realpath $BASEDIR)
 sudo apt-get update -y
 sudo apt-get install tcl -y
 
+# fix permissions & add user redis
+sudo mkdir /var/lib/redis
+sudo adduser --system --group --no-create-home redis
+sudo chown redis:redis /var/lib/redis
+sudo chmod 770 /var/lib/redis
+
+
 sudo apt install -y redis-server
 
 echo "DONE 05-redis.sh"
